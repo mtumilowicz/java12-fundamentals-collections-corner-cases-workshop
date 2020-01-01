2478105f3ccf23139b4f85779d0cb28860a2f588
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-01-01 16:54:18 +0100

    MapAlgorithmProblemAnswer.groovy MapAlgorithmProblemWorkshop.groovy

--- src/test/groovy/answers/views/MapAlgorithmProblemAnswer.groovy
+++ src/test/groovy/answers/views/MapAlgorithmProblemAnswer.groovy
@@ -6,16 +6,16 @@ import java.util.stream.Collectors
 
 class MapAlgorithmProblemAnswer extends Specification {
     /*
-    Given two maps mapA and mapB, fnd the keys of mapB
+    Given two maps mapA and mapB, find the keys of mapB
     corresponding to values that occur in both maps
-    var mapA = Map.of(1, "a", 2, "b", 3, "c")
-    var mapB = Map.of(5, "a", 6, "d", 7, "c")
+    var mapA = Map.of(1, 'a', 2, 'b', 3, 'c')
+    var mapB = Map.of(5, 'a', 6, 'd', 7, 'c')
 
     Result should be a answers.set containing: [5, 7]
      */
 
-    def mapA = Map.of(1, "a", 2, "b", 3, "c")
-    def mapB = Map.of(5, "a", 6, "d", 7, "c")
+    def mapA = Map.of(1, 'a', 2, 'b', 3, 'c')
+    def mapB = Map.of(5, "a", 6, 'd', 7, 'c')
 
     def 'find using stream API'() {
         given:
--- src/test/groovy/workshop/views/MapAlgorithmProblemWorkshop.groovy
+++ src/test/groovy/workshop/views/MapAlgorithmProblemWorkshop.groovy
@@ -6,42 +6,27 @@ import java.util.stream.Collectors
 
 class MapAlgorithmProblemWorkshop extends Specification {
     /*
-    Given two maps mapA and mapB, fnd the keys of mapB
+    Given two maps mapA and mapB, find the keys of mapB
     corresponding to values that occur in both maps
-    var mapA = Map.of(1, "a", 2, "b", 3, "c")
-    var mapB = Map.of(5, "a", 6, "d", 7, "c")
+    var mapA = Map.of(1, 'a', 2, 'b', 3, 'c')
+    var mapB = Map.of(5, 'a', 6, 'd', 7, 'c')
 
     Result should be a answers.set containing: [5, 7]
      */
 
-    def mapA = Map.of(1, "a", 2, "b", 3, "c")
-    def mapB = Map.of(5, "a", 6, "d", 7, "c")
+    def mapA = Map.of(1, 'a', 2, 'b', 3, 'c')
+    def mapB = Map.of(5, "a", 6, 'd', 7, 'c')
 
     def 'find using stream API'() {
-        given:
-        def valuesOfA = mapA.values() as Set
-
         when:
-        Set<Integer> result = mapB.entrySet().stream()
-                .filter { valuesOfA.contains(it.value) }
-                .map { it.key }
-                .collect(Collectors.toList())
+        Set<Integer> result = null // hint: use values()
 
         then:
         result == [5, 7] as Set
     }
 
     def 'find using collection API'() {
-        given:
-        def valuesOfA = new HashSet<>(mapA.values())
-        def copyOfB = new HashMap<>(mapB)
-        def valuesOfB = copyOfB.values()
-
-        when:
-        valuesOfB.retainAll(valuesOfA)
-
-        then:
-        copyOfB.keySet() == [5, 7] as Set
+        // hint: use retainAll and keySet
     }
 
 }
